06e17c9867b9cb6d68a3ac5497ab95f1a8943707
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2023-06-23 12:57:12 -0400

    [UPDATE] Created Counter Component with Multiple Independent Instances in App

--- native-useState/src/App.jsx
+++ native-useState/src/App.jsx
@@ -1,7 +1,28 @@
 import {useState} from "react";
 
+// Single Counter Instance
+const Counter = () => {
+  const [getCount, setCount] = useState(10);
+
+  const addOne = () => {
+    setCount(getCount + 1);
+  }
+
+  return <div className="App">
+    <button onClick={addOne}>Count = {getCount}</button>
+  </div>;
+}
+
+// Multiple Independent Counter Instances
 const App = () => {
-  return <div className="App"></div>
+  return (
+    <div>
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+    </div>
+  )
 }
 
 export default App;
